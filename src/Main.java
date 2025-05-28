2b707b2f971c8378b5507e90f2ef5238006ef0aa
Vinicius Silva dos Santos <vinciussantos15@gmail.com>
2025-05-28 20:58:59 -0300

    Adicionando leitor de arquivos

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.io.FileNotFoundException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws FileNotFoundException {
+        int[] vetor = LeitorArquivo.lerInteiros("arquivos/inteiros.txt");
+
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println(vetor[i]);
+        }
     }
 }
