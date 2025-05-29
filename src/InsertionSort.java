b92f4e370781d18f3c91e08594cdb2c397430be0
Vinicius dos Santos <vinciussantos15@gmail.com>
2025-05-29 20:19:07 -0300

    ajuste

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,5 +1,5 @@
 public class InsertionSort {
-    public static int[] index(int[] vetor){
-        return vetor;
+    public static void index(int[] vetor){
+        // Nas classes de Ordenação além do algoritmo a impressão dos resultados serão colocadas dentro destas classes isoladas tb
     }
 }
